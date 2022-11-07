be21e68943bac0ddca203cf7537c1750135d1f34
Elly Wong <ellyw1002@gmail.com>
2022-11-06 19:53:23 -0500

    fix retry

--- src/api/takeScreenshot.api.js
+++ src/api/takeScreenshot.api.js
@@ -1,21 +1,36 @@
+function takeScreenshotWithRetry(url, retries = 3) {
+
+  return fetch(url).then(async (response) => {
+    if (!response.ok) {
+      // retry or throw error
+      if (retries > 0) {
+        console.log('Retry: ' + retries);
+        // setTimeout(() => {
+        //   return takeScreenshotWithRetry(url, retries - 1);
+        // }, 1000);
+        return await takeScreenshotWithRetry(url, retries - 1);
+      } else {
+        throw new Error(response.status);
+      }
+    }
+    return response;
+  }).catch(error => console.log(error.message));
+}
+
 export async function takeScreenshot(stockList = []) {
   let stockScreenshots = [];
   if (!stockList || stockList.length === 0) {
     throw 'stock is required';
   }
   for (const item of stockList) {
-    let retries = 3;
-    while (retries > 0) {
-      try {
-        console.log('before screenshot: ', item.symbol);
-        const response = await fetch(`api/takeScreenshot?term=${item.symbol}`);
-        stockScreenshots[item.symbol] = await response.json();
-        break;
-        // console.log('stockScreenshot: ', stockScreenshots[i]);
-      } catch (err) {
-        console.log('# retries: ', retries);
-        retries--;
-      }
+    try {
+      console.log('before screenshot: ', item.symbol);
+      const response = await takeScreenshotWithRetry(`api/takeScreenshot?term=${item.symbol}`, 3);
+      console.log('response: ', response);
+      stockScreenshots[item.symbol] = await response.json();
+    } catch (error) {
+      console.log(`failed after 3 retries for stock ${item.symbol}`);
+      break;
     }
   };
   console.log('returning from api: ', stockScreenshots);
