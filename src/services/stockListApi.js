98f052892b383b1359c9630b96927872bcb601bb
Elly Wong <ellyw1002@gmail.com>
2023-10-16 22:29:12 -0400

    Revert "test"
    This reverts commit 3cf0a7d939214c5f901c9c7810c7a2b23a25a36e.

--- src/services/stockListApi.js
+++ src/services/stockListApi.js
@@ -1,4 +1,3 @@
-import { listItemTextClasses } from "@mui/material";
 import { api } from "./api";
 
 export const stockListApi = api
@@ -19,7 +18,7 @@ export const stockListApi = api
       resetStock: build.mutation({
         async queryFn(accessLevelRequests, _queryApi, _extraOptions, baseQuery) {
           const { stockList } = accessLevelRequests;
-          let isFirst = true;
+          const isFirst = true;
           for (const stock of stockList) {
             const response = await baseQuery({
               url: `resetStock?term=${stock.id}&isFirst=${isFirst}`,
@@ -43,7 +42,7 @@ export const stockListApi = api
         async queryFn(accessLevelRequests, _queryApi, _extraOptions, baseQuery) {
           const { stockList, time } = accessLevelRequests;
           for (const stock of stockList) {
-            let retries = 3;
+            const retries = 3;
             let screenshotState = JSON.parse(window.localStorage.getItem(`${stock.id}`)) || {
               morning: false,
               afternoon: false,
@@ -73,7 +72,7 @@ export const stockListApi = api
         async queryFn(accessLevelRequests, _queryApi, _extraOptions, baseQuery) {
           const { stockList } = accessLevelRequests;
           for (const stock of stockList) {
-            let retries = 3;
+            const retries = 3;
             let screenshotState = JSON.parse(window.localStorage.getItem(`${stock.id}`)) || {
               night: false
             };
