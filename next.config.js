b21e7392abdabc7ab21b7cf8c83177882ea8a59b
Elly Wong <ellyw1002@gmail.com>
2023-10-16 22:28:51 -0400

    test2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.exe$/i,
+      use: 'raw-loader',
+    },);
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
